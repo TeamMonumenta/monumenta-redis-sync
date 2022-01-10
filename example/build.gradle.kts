5c9c44423d844550ea10527718d39e1f8cfe9c73
Byron Marohn <Combustible@live.com>
2022-01-10 05:05:48 +0000

    Cleanup
    Signed-off-by: Byron Marohn <Combustible@live.com>

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
     compileOnly(project(":redissync"))
-    compileOnly("com.destroystokyo.paper:paper-api:1.15.2-R0.1-SNAPSHOT")
+    compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
     compileOnly("net.md-5:bungeecord-api:1.15-SNAPSHOT")
     compileOnly("net.md-5:bungeecord-api:1.15-SNAPSHOT")
 }

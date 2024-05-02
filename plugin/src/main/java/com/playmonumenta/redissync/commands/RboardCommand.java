298139cbe4f8599d1664a8f8f8b9fd7e531d96a8
NobodyPi <officialnobodypi@gmail.com>
2024-05-02 13:54:00 -0700

    fix imports

--- plugin/src/main/java/com/playmonumenta/redissync/commands/RboardCommand.java
+++ plugin/src/main/java/com/playmonumenta/redissync/commands/RboardCommand.java
@@ -22,7 +22,6 @@ import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;

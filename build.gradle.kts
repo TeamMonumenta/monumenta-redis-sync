0bda0617030d1952c3403be3379e676ad863711d
Floweynt <guywithanaxe42@gmail.com>
2024-06-22 00:48:14 -0500

    feat: 1.20.4 version adapters
    Added support for 1.20.4 using the proper version adapter.

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,5 +19,6 @@ monumenta {
 	versionAdapter("adapter_v1_18_R2", "1.18.2")
 	versionAdapter("adapter_v1_19_R2", "1.19.3")
 	versionAdapter("adapter_v1_19_R3", "1.19.4")
+	versionAdapter("adapter_v1_20_R3", "1.20.4")
 	javaSimple(":redissync-example")
 }

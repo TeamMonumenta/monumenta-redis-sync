c8d17f9e05af18d66ab99cdaf03cba9dab3eca1c
karpandsmeargle <133045905+karpandsmeargle@users.noreply.github.com>
2024-08-21 20:27:15 -0400

    test(lock): Add tests to ensure exceptions are properly thrown on incorrect unlocks.

--- example/src/main/java/com/playmonumenta/redissync/example/redislockexamples/RedisLockExampleCommands.java
+++ example/src/main/java/com/playmonumenta/redissync/example/redislockexamples/RedisLockExampleCommands.java
@@ -8,6 +8,7 @@ public class RedisLockExampleCommands {
 		new CommandAPICommand("redislockexamples")
 			.withPermission("monumenta.redislockexamples")
 			.withSubcommand(ToyExampleCommand.command(plugin))
+			.withSubcommand(ExceptionsExampleCommand.command(plugin))
 			.register();
 	}
 }

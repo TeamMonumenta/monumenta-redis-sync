20242eb7e378c8951bdb0a95013aae3c9b6c0551
Floweynt <guywithanaxe42@gmail.com>
2024-09-06 16:34:43 -0500

    feed dog

--- adapter_v1_20_R3/src/main/java/com/playmonumenta/redissync/adapters/VersionAdapter_v1_20_R3.java
+++ adapter_v1_20_R3/src/main/java/com/playmonumenta/redissync/adapters/VersionAdapter_v1_20_R3.java
@@ -2,7 +2,6 @@ package com.playmonumenta.redissync.adapters;
 
 import ca.spottedleaf.dataconverter.minecraft.MCDataConverter;
 import ca.spottedleaf.dataconverter.minecraft.datatypes.MCTypeRegistry;
-import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;

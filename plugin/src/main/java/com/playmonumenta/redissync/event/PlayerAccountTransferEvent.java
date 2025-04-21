d8f62177a889c2ae3db3fe25ea82afea1926a8ad
Timothy Southwick <NickNackGus@gmail.com>
2025-04-21 16:51:24 -0400

    Added docstrings

--- plugin/src/main/java/com/playmonumenta/redissync/event/PlayerAccountTransferEvent.java
+++ plugin/src/main/java/com/playmonumenta/redissync/event/PlayerAccountTransferEvent.java
@@ -19,10 +19,18 @@ public class PlayerAccountTransferEvent extends PlayerEvent {
 		mOldName = oldName;
 	}
 
+	/**
+	 * Gets the player's previous UUID before they transferred their account
+	 * @return The player's previous UUID
+	 */
 	public UUID getOldId() {
 		return mOldId;
 	}
 
+	/**
+	 * Gets the player's previous name before they transferred their account
+	 * @return The player's previous name
+	 */
 	public String getOldName() {
 		return mOldName;
 	}

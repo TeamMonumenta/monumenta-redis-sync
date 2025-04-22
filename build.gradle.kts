750e0dfa6835e48db84fef37429a53f719094d89
Timothy Southwick <NickNackGus@gmail.com>
2025-04-21 20:22:55 -0400

    Updated Monumenta gradle config version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 
 plugins {
-	id("com.playmonumenta.gradle-config") version "2.2+"
+	id("com.playmonumenta.gradle-config") version "2.+"
 }
 
 val mixinapi = libs.mixinapi
